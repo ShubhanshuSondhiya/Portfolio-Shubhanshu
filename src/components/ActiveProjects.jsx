c7aff23f64713f7fac1d0521020cb9037933bc15
Shubhanshu Sondhiya <shubhanshusondhiya@gmail.com>
2025-01-09 09:22:47 +0530

    fixed folder structure

---
+++ src/components/ActiveProjects.jsx
@@ -0,0 +1,10 @@
+
+const ActiveProjects = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ActiveProjects
---
+++ src/components/PageSwitch.jsx
@@ -0,0 +1,10 @@
+
+const PageSwitch = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PageSwitch
